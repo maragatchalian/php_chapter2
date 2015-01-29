515b2ea390d7486138d30fac638953705632dd32
mara <maragatchalian@github.com>
2015-01-29 14:55:10 +0800

    Created statements.php file

---
+++ statements.php
@@ -0,0 +1,18 @@
+<html>
+<head> <title> "What does this thing do?" </head></title>
+<body>
+</body>
+</html>
+
+<?php
+
+echo "Hello, World";
+myFunction(42, "Mara");
+$a = 1;
+$name = "Mara Gatchalian";
+$b = $a / 25.0;
+if ($a == $b){
+	echo "Rhyme? And Reason?";
+}
+?>
+
