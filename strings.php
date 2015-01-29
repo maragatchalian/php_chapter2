a747db78d71a9783c175d295863c16b9cd5152b5
mara <maragatchalian@github.com>
2015-01-29 15:18:18 +0800

    Added a new file called strings.php

---
+++ strings.php
@@ -0,0 +1,14 @@
+<?php
+#Sample php script for srings
+#Difference between a double and single quote
+
+$name = "Mara";
+
+//single quote
+echo "single quote\n";
+echo '$name';
+
+//double quote
+echo "double quote\n";
+echo "$name";
+?> 
